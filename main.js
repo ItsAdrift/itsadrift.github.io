728bcf43ae61994a9cb6a667972ef7bd910ae4cd
ItsAdrift <joshuaking269@gmail.com>
2022-10-01 17:36:08 +1000

    Improved Bread+Wrap Count, totals added

--- main.js
+++ main.js
@@ -1,35 +1,73 @@
-var survey_options = document.getElementById('survey_options');
-var holder = document.getElementById('holder');
-var add_more_fields = document.getElementById('add_more_fields');
-var remove_fields = document.getElementById('remove_fields');
+var so = document.getElementById('bread_survey_options');
+var h = document.getElementById('bread_holder');
+var add_more_fields = document.getElementById('bread_add_more_fields');
+var rf = document.getElementById('bread_remove_fields');
 
 var submitBtn = document.getElementById('submit');
 
 add_more_fields.onclick = function(){
-  var newField = document.createElement('input');
-  newField.setAttribute('type','text');
-  newField.setAttribute('name','survey_options[]');
-  newField.setAttribute('class', 'border-2 rounded border-gray-500 dark:border-green-500 dark:border-3 dark:text-black p-2 sm:w-48 md:w-96 mt-5')
-  newField.setAttribute('placeholder','Other Amounts');
-  holder.appendChild(newField);
+  var f = document.createElement('input');
+
+  f.setAttribute('type','number');
+  f.setAttribute('name','bread_survey_options[]');
+  f.setAttribute('class', 'border-2 rounded border-gray-500 dark:text-black p-2 sm:w-48 md:w-96 mt-5')
+  f.setAttribute('placeholder','Other Amounts');
+  h.appendChild(f);
   return false;
 }
 
-remove_fields.onclick = function(){
-  var input_tags = survey_options.getElementsByTagName('input');
+rf.addEventListener("click", function(){
+  var input_tags = so.getElementsByTagName('input');
+  console.log()
   if(input_tags.length > 1) {
-    holder.removeChild(input_tags[(input_tags.length) - 1]);
+    var i = 1;
+    while (input_tags[(input_tags.length)-i].name == "") {
+      i++;
+    } 
+    input_tags[(input_tags.length) - i].parentNode.removeChild(input_tags[(input_tags.length) - i]);
   }
   return false;
+}, false);
+
+
+submitBtn.addEventListener("click", function() {
+	let breadSticks = 0;
+	breadSticks = document.getElementById("bread_fullBoxes").value * 85;
+	for (var i = 0; i < document.getElementsByName('bread_survey_options[]').length; i++) {
+		breadSticks += validate(document.getElementsByName('bread_survey_options[]')[i].value);
+	}
+	document.getElementById("bread_freezer").innerText = ""+breadSticks;
+  total();
+}, false);
+
+function isEmpty(str) {
+  return !str.trim().length;
 }
 
-document.getElementById("result").style.display = 'none';
-submitBtn.onclick = function() {
-	let breadSticks = 0;
-	breadSticks = document.getElementById("fullBoxes").value * 85;
-	for (var i = 0; i < document.getElementsByName('survey_options[]').length; i++) {
-		breadSticks += Number.parseInt(document.getElementsByName('survey_options[]')[i].value);
-	}
-	document.getElementById("result").innerText = "Result: "+breadSticks;
-	document.getElementById("result").style.display = 'block';
+function validate(str) {
+  if (isEmpty(toString(str))) {
+      return 0;
+  } else {
+      return +str;
+  }
 }
+
+function total() {
+  var freezer = +document.getElementById("bread_freezer").innerText;
+
+  var front = validate(document.getElementById("bread_front").value);
+  document.getElementById("result_bread_front").innerText = front;
+
+  var retarder = 0;
+  var v = document.getElementById("bread_retarder").value;
+  if (!isEmpty(v)) {
+    retarder = +v;
+  } else {
+    retarder = document.getElementById("bread_retarder").getAttribute("placeholder");
+  }
+  document.getElementById("bread_proofer").innerText = +retarder;
+
+  document.getElementById("bread_total").innerText= +(Number.parseInt(freezer)+Number.parseInt(front)+Number.parseInt(retarder));
+
+}
+
--- wrapCount.js
+++ wrapCount.js
@@ -25,7 +25,7 @@ remove_fields.onclick = function(){
 }
 
 //document.getElementById("result").style.display = 'none';
-submitBtn.onclick = function() {
+submitBtn.addEventListener("click",  function() {
     /*
         FREEZER
     */
@@ -67,8 +67,18 @@ submitBtn.onclick = function() {
 
     document.getElementById("front").innerText = ""+fWraps;
 
+    /* GF Retarder */
+
+    let gfrWraps = 0;
+    gfrWraps += (validate(+document.getElementById("gfrFull").value*4));
+    gfrWraps += validate(+document.getElementById("gfrNotFull").value);
+
+    document.getElementById("wrap_proofer").innerText = ""+gfrWraps;
+    
 	document.getElementById("result").style.display = 'block';
-}
+    
+    totalWraps(wraps, fWraps, gfWraps, gfrWraps);
+}, false);
 
 function isEmpty(str) {
     return !str.trim().length;
@@ -87,23 +97,49 @@ function validate(str) {
     }
 }
 
+function totalWraps(wraps, frontWraps, gfWraps, gfWrapsRet) {
+    document.getElementById("wrap_total").innerText = sumAll(wraps, frontWraps, gfWraps, gfWrapsRet);
+}
+
+function sumAll() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  }
+
 // create an array of objects with the id, trigger element (eg. button), and the content element
 const tabElements = [
     {
+        id: 'bread',
+        triggerEl: document.querySelector('#bread-tab'),
+        targetEl: document.querySelector('#bread')
+    },
+    {
         id: 'profile',
-        triggerEl: document.querySelector('profile-tab'),
-        targetEl: document.querySelector('profile')
+        triggerEl: document.querySelector('#profile-tab'),
+        targetEl: document.querySelector('#profile')
     },
     {
         id: 'dashboard',
         triggerEl: document.querySelector('#dashboard-tab'),
         targetEl: document.querySelector('#dashboard')
+    },
+    {
+        id: 'gf',
+        triggerEl: document.querySelector('#gf-tab'),
+        targetEl: document.querySelector('#gf')
     }
 ];
-
+/*
+<li class="mr-2" role="presentation">
+                    <button class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300 active" id="bread-tab" data-tabs-target="#bread" type="button" role="tab" aria-controls="bread" aria-selected="true">Bread</button>
+                </li>
+*/
 // options with default values
 const options = {
-    defaultTabId: 'profile',
+    defaultTabId: 'bread',
     activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
     inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: () => {
@@ -112,5 +148,5 @@ const options = {
 };
 
 const tabs = new Tabs(tabElements, options);
-tabs.show('profile')
+tabs.show('bread')
 
